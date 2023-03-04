2c0adc9a632482ead4038c1f03741380d62e0e9c
Matteo Castellucci <matteo.castellucci@outlook.com>
2023-03-04 19:58:22 +0100

    fix: add different usernames and passwords for all services, add ditto service in generated configuration

--- install.demo.sh
+++ install.demo.sh
@@ -2,47 +2,59 @@
 
 set -euo pipefail;
 
-if [[ "$#" != "2" ]]; then
-    echo "Needed two parameters, a username and a password";
+if [[ "$#" != "6" ]]; then
+    echo "Needed six parameters, three pairs of usernames and passwords";
     exit 1;
 fi;
 
-USERNAME=$1;
-PASSWORD=$2;
+DB_USERNAME=$1;
+DB_PASSWORD=$2;
+RMQ_USERNAME=$3;
+RMQ_PASSWORD=$4;
+DT_USERNAME=$5;
+DT_PASSWORD=$6;
 
 MICROSERVICES=("users" "items" "carts");
 
 for INDEX in ${!MICROSERVICES[@]}; do
     MICROSERVICE_NAME=${MICROSERVICES[$INDEX]};
-    SERVER_PORT_NUMBER=$(expr 8081 + $INDEX);
+    SERVER_PORT_NUMBER=$(expr 8881 + $INDEX);
     touch ./$MICROSERVICE_NAME/application.conf;
     cat > ./$MICROSERVICE_NAME/application.conf <<-EOF
 		repository {
 		    dataSourceClassName = org.postgresql.ds.PGSimpleDataSource
 		    dataSource {
-		        user = $USERNAME
-		        password = $PASSWORD
+		        user = $DB_USERNAME
+		        password = $DB_PASSWORD
 		        databaseName = $MICROSERVICE_NAME
 		        portNumber = 5432
-		        serverName = "0.0.0.0"
+		        serverName = 0.0.0.0
 		    }
 		    connectionTimeout = 30000
 		}
 		server {
 		    portNumber = $SERVER_PORT_NUMBER
-		    hostName = "0.0.0.0"
+		    hostName = 0.0.0.0
 		}
 		messageBroker {
-		    username = $USERNAME
-		    password = $PASSWORD
+		    username = $RMQ_USERNAME
+		    password = $RMQ_PASSWORD
 		    virtualHost = "/"
 		    portNumber = 5672
-		    hostName = "0.0.0.0"
+		    hostName = 0.0.0.0
+		}
+		ditto {
+		    hostName = 0.0.0.0
+		    portNumber = 8080
+		    username = $DT_USERNAME
+		    password = $DT_PASSWORD
+		    namespace = io.github.pervasivecats
+		    thingModel = "https://raw.githubusercontent.com/pervasive-cats/toys-store-carts/main/cart.jsonld"
 		}
 	EOF
 done;
 
-docker compose up -d;
+docker compose -f ./docker-compose.demo.yml up -d;
 
 echo "Services started";
     
--- install.sh
+++ install.sh
@@ -2,13 +2,17 @@
 
 set -euo pipefail;
 
-if [[ "$#" != "2" ]]; then
-    echo "Needed two parameters, a username and a password";
+if [[ "$#" != "6" ]]; then
+    echo "Needed six parameters, three pairs of usernames and passwords";
     exit 1;
 fi;
 
-USERNAME=$1;
-PASSWORD=$2;
+DB_USERNAME=$1;
+DB_PASSWORD=$2;
+RMQ_USERNAME=$3;
+RMQ_PASSWORD=$4;
+DT_USERNAME=$5;
+DT_PASSWORD=$6;
 
 docker compose -f ./ditto/docker-compose.yml up -d;
 
@@ -31,24 +35,32 @@ for INDEX in ${!MICROSERVICES[@]}; do
 		repository {
 		    dataSourceClassName = org.postgresql.ds.PGSimpleDataSource
 		    dataSource {
-		        user = $USERNAME
-		        password = $PASSWORD
+		        user = $DB_USERNAME
+		        password = $DB_PASSWORD
 		        databaseName = $MICROSERVICE_NAME
 		        portNumber = $POSTGRES_PORT_NUMBER
-		        serverName = "0.0.0.0"
+		        serverName = 0.0.0.0
 		    }
 		    connectionTimeout = 30000
 		}
 		server {
 		    portNumber = $SERVER_PORT_NUMBER
-		    hostName = "0.0.0.0"
+		    hostName = 0.0.0.0
 		}
 		messageBroker {
-		    username = $USERNAME
-		    password = $PASSWORD
+		    username = $RMQ_USERNAME
+		    password = $RMQ_PASSWORD
 		    virtualHost = "/"
 		    portNumber = 5672
-		    hostName = "0.0.0.0"
+		    hostName = 0.0.0.0
+		}
+		ditto {
+		    hostName = 0.0.0.0
+		    portNumber = 8080
+		    username = $DT_USERNAME
+		    password = $DT_PASSWORD
+		    namespace = io.github.pervasivecats
+		    thingModel = "https://raw.githubusercontent.com/pervasive-cats/toys-store-carts/main/cart.jsonld"
 		}
 	EOF
 done;
